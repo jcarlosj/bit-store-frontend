2b88b4626f0b82bbf76e79613631f50b06f93517
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-09-13 17:02:20 -0500

    [+] products-header.component.*: Select para ordenar (asc|desc) usando ngModel

--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+enum SortOrder {
+  Asc = 'asc',
+  Desc = 'desc'
+}
 
 @Component({
   selector: 'app-products-header',
   standalone: true,
-  imports: [],
-  template: `
-    <p>
-      products-header works!
-    </p>
-  `,
-  styles: ``
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './products-header.component.html',
+  styleUrl: './products-header.component.css'
 })
 export class ProductsHeaderComponent {
+  sortOrder: SortOrder = SortOrder.Desc; 
 
+  onSortChange( event: any ) {
+    this.sortOrder = event.target.value;
+  }
 }
