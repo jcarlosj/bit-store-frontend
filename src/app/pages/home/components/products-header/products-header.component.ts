567504c5de064b699bae88a005313c777757e4c5
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-09-13 16:15:26 -0500

    [+] ng g c pages/home/components/products-header

---
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-header',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      products-header works!
+    </p>
+  `,
+  styles: ``
+})
+export class ProductsHeaderComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ProductsHeaderComponent } from './components/products-header/products-header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    ProductsHeaderComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
