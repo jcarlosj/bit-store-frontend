0aceab456520754805ceff3c70b58e7cd3da64e6
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-09-19 14:49:50 -0500

    [+] ng g c pages/home/components/product-card

---
+++ src/app/pages/home/components/product-card/product-card.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      product-card works!
+    </p>
+  `,
+  styles: ``
+})
+export class ProductCardComponent {
+
+}
