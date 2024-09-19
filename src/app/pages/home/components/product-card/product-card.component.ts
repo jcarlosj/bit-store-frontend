b8d151e0aeafe6140495a802183343e154bfc435
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-09-19 15:18:57 -0500

    [+] product-card.component.*: HTML & CSS (Estructura) [+] home.component.*: Implementar ProductCardComponent

--- src/app/pages/home/components/product-card/product-card.component.ts
+++ src/app/pages/home/components/product-card/product-card.component.ts
@@ -1,15 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [],
-  template: `
-    <p>
-      product-card works!
-    </p>
-  `,
-  styles: ``
+  imports: [ CommonModule, MatIconModule ],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.css'
 })
 export class ProductCardComponent {
 
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { ProductsHeaderComponent } from './components/products-header/products-header.component';
 import { ProductsFilterComponent } from './components/products-filter/products-filter.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     ProductsHeaderComponent,
-    ProductsFilterComponent
+    ProductsFilterComponent,
+    ProductCardComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
