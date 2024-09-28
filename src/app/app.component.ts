35773c10bccc03563d8067604f42618482527109
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-09-28 15:44:52 -0500

    [+] (header, product-card).component.*: Despliegue de Iconos (FontAwesome)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 import { HeaderComponent } from './components/layout/header/header.component';
 
 @Component({
--- src/app/pages/home/components/product-card/product-card.component.ts
+++ src/app/pages/home/components/product-card/product-card.component.ts
@@ -1,13 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [ CommonModule ],
+  imports: [ CommonModule, FontAwesomeModule ],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css'
 })
 export class ProductCardComponent {
-
+  faCartPlus = faCartPlus;
 }
