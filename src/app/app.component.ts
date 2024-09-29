a3bc7f66a622c6497a4b3d07f3fd9370d2bf4ad3
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-09-29 15:42:42 -0500

    [+] ng g c components/layout/footer & Estructura HTML & CSS [$] (header, product-filter, products-header).component*: CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styles: [],
