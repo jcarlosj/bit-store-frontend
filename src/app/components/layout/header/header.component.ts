2359f19d490e5b6736904005e2e9e2d26a5acf76
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-10-10 07:57:29 -0500

    [+] app.routes: Define rutas /login /register [+] (login, register).component*: HTML & CSS

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faBars, faXmark, faCartShopping } from '@fortawesome/free-solid-svg-icons';
@@ -6,7 +7,7 @@ import { faBars, faXmark, faCartShopping } from '@fortawesome/free-solid-svg-ico
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ FontAwesomeModule ],
+  imports: [ FontAwesomeModule, RouterLink ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
